51e8f9caa3bde6989fea7a48a14e402aa9722e00
tyf <448371352@qq.com>
2019-12-19 16:56:50 +0800

    first commit

---
+++ src/main/java/com/tyf/logtest/LogtestApplication.java
@@ -0,0 +1,13 @@
+package com.tyf.logtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tyf/logtest/LogtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyf.logtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
